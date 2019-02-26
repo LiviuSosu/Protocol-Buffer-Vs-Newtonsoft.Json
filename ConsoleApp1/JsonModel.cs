9af4ff3bd1d50bb2b73b41546865a8533ee3d34b
Liviu Sosu <sosuliviu@gmail.com>
2019-02-26 09:57:58 +0200

    Implemented comparison.

---
+++ ConsoleApp1/JsonModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class BookInfoJson
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int PageCount { get; set; }
+
+        public AuthorJson Author { get; set; }
+    }
+
+    class AuthorJson
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿using Google.Protobuf;
+using Google.Protobuf.Examples.AddressBook;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using static Google.Protobuf.Examples.AddressBook.Books.Types;
+using static Google.Protobuf.Examples.AddressBook.Books.Types.BookInfo.Types;
 
 namespace ConsoleApp1
 {
@@ -6,8 +14,81 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Done");
+            int dataSize = 100000;
+            //SerializeProtoBuff(dataSize);
+            SerializeJson(dataSize);
+
+            Console.WriteLine("Finished!");
             Console.ReadKey();
+        }
+
+        private static void SerializeProtoBuff(int dataSize)
+        {
+            BookInfo bookInfo;
+            Books books = new Books();
+            //generating data
+            for (int i = 1; i <= dataSize; i++)
+            {
+                bookInfo = new BookInfo
+                {
+                    Id = i,
+                    Author = new Author() { Name = "Author name " + i },
+                    Title = "Title " + i,
+                    PageCount = i,
+                };
+
+                books.Books_.Add(bookInfo);
+            }
+
+            var sw = new Stopwatch();
+            sw.Start();
+
+            //make sure to adapt file path in order to work properly
+            string fileName = @"C:\Users\lsosu\Work\Protobuf\ConsoleApp1\ConsoleApp1\data.dat";
+
+            using (var output = File.Create(fileName))
+            {
+                books.WriteTo(output);
+            }
+
+            using (var input = File.OpenRead(fileName))
+            {
+                books = Books.Parser.ParseFrom(input);
+            }
+            sw.Stop();
+            long elapsedMilliseconds = sw.ElapsedMilliseconds;
+        }
+
+        private static void SerializeJson(int dataSize)
+        {
+            BookInfoJson bookInfo;
+            var books = new List<BookInfoJson>();
+
+            //generating data
+            for (int i = 1; i <= dataSize; i++)
+            {
+                bookInfo = new BookInfoJson
+                {
+                    Id = i,
+                    Author = new AuthorJson() { Name = "Author name " + i },
+                    Title = "Title " + i,
+                    PageCount = i
+                };
+
+                books.Add(bookInfo);
+            }
+
+            var sw = new Stopwatch();
+            sw.Start();
+
+            //make sure to adapt file path in order to work properly
+            string fileName = @"C:\Users\lsosu\Work\Protobuf\ConsoleApp1\ConsoleApp1\data.json";
+
+            File.WriteAllText(fileName, JsonConvert.SerializeObject(books));
+
+            books = JsonConvert.DeserializeObject<List<BookInfoJson>>(File.ReadAllText(fileName));
+            sw.Stop();
+            long elapsedMilliseconds = sw.ElapsedMilliseconds;
         }
     }
 }
